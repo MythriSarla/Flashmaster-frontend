2eb9e99586abb8f24ada25d86a367fa1489ea24a
Mythri Sarla <your@email.com>
2026-04-28 02:14:11 +0530

    fix: point API to Render backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
-
+const API = axios.create({ baseURL: 'https://flashmaster-backened-1.onrender.com/api' });
 API.interceptors.request.use(req => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
-
 export default API;
